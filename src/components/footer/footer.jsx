a3bb04f5d6c9c5ae27f61fcc20f4a03b46e5d6fc
Haeun Kim <haeun159951@gmail.com>
2021-12-20 15:47:30 -0500

    login footer header

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const footer = () => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Code your Dream</p>
+  </footer>
+);
+
+export default footer;
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,13 +1,18 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase';
+
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_DB_URL,
-  databaseURL: REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_FIREBASE_DB_URL);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
