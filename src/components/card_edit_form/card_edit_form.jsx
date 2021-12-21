9d19e5a7a7d09f1656c4bc1162a7c234ed0c9e1a
Haeun Kim <haeun159951@gmail.com>
2021-12-21 17:59:07 -0500

    add update delete function

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import styles from './card_edit_form.module.css';
 import Button from '../button/button';
 import ImageFileInputButton from '../image_file_input_button/image_file_input_button';
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
   const { name, company, title, email, message, theme, fileName, fileURL } =
     card;
 
+  const onChange = (event) => {
+    if (event.currentTarget == null) {
+      return;
+    }
+
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
   const onSubmit = () => {};
   return (
     <form className={styles.form}>
@@ -14,35 +25,45 @@ const CardEditForm = ({ card }) => {
         type='text'
         name='name'
         value={name}
+        onChange={onChange}
       ></input>
       <input
         className={styles.input}
         type='text'
         name='company'
         value={company}
+        onChange={onChange}
       ></input>
-      <select className={styles.select} name='theme' value={theme}>
+      <select
+        className={styles.select}
+        name='theme'
+        value={theme}
+        onChange={onChange}
+      >
         <option value='light'>light</option>
-        <option value='night'>dark</option>
-        <option value='dark'>colorful</option>
+        <option value='dark'>dark</option>
+        <option value='colorful'>colorful</option>
       </select>
       <input
         className={styles.input}
         type='text'
         name='title'
         value={title}
+        onChange={onChange}
       ></input>
       <input
         className={styles.input}
         type='text'
         name='email'
         value={email}
+        onChange={onChange}
       ></input>
       <textarea
         className={styles.textarea}
         type='text'
         name='message'
         value={message}
+        onChange={onChange}
       ></textarea>
       <div className={styles.fileInput}>
         <ImageFileInputButton />
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,9 +7,9 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
-  //card object
-  const [cards, setCards] = useState([
-    {
+  //card object  - not array
+  const [cards, setCards] = useState({
+    1: {
       id: '1',
       name: 'haeun',
       company: 'BMO',
@@ -20,7 +20,7 @@ const Maker = ({ authService }) => {
       filename: 'haeun',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'hailey',
       company: 'Samsung',
@@ -31,7 +31,7 @@ const Maker = ({ authService }) => {
       filename: 'haeun',
       fileURL: null,
     },
-    {
+    3: {
       id: '3',
       name: 'Kim',
       company: 'Alpha Labs',
@@ -42,7 +42,7 @@ const Maker = ({ authService }) => {
       filename: 'haeun',
       fileURL: null,
     },
-  ]);
+  });
 
   const history = useHistory();
 
@@ -58,16 +58,36 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrupdateCard = (card) => {
+    //js - object feature - for good performance
+    // OLDWAY: const updated = { ...cards };
+    // updated[card.id] = card;
+    //setCards(updated);
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrupdateCard}
+          updateCard={createOrupdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
